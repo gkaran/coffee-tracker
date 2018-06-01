b8dfddc105d95a6057ea3143db48fbcf0769af5a
gkaran <gkaran89@gmail.com>
2018-06-01 11:21:30 -0400

    partial: deployment and ci

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA5Sif7HCAxRmv2jNYXGl9ept1pXjDp8c4',
+    authDomain: 'fts-coffee.firebaseapp.com',
+    databaseURL: 'https://fts-coffee.firebaseio.com',
+    projectId: 'fts-coffee',
+    storageBucket: '',
+    messagingSenderId: '660167096206'
+  }
 };
