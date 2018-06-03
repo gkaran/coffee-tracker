d35ade3c4236b4d291b5c35f4b4ffe6554e129b7
argy <cargy@protonmail.ch>
2018-06-03 08:42:03 +0300

    create new update name modal dialog component

---
+++ src/app/modals/update-name-modal/update-name-modal.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UpdateNameModalComponent } from './update-name-modal.component';
+
+describe('UpdateNameModalComponent', () => {
+  let component: UpdateNameModalComponent;
+  let fixture: ComponentFixture<UpdateNameModalComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UpdateNameModalComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UpdateNameModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
